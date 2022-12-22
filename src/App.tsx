8f7179848b12c5aad7ada35bffa209790593d426
leobhmg45 <leobhmg45@gmailo.com>
2022-12-22 13:23:48 -0200

    inicio das areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'
+import logo from './assets/powered.png'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header className={styles.headerContainer}>
+        <img src={logo} alt="logo" width={150} />
       </header>
-    </div>
-  );
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          left side
+        </div>
+        <div className={styles.rightSide}>
+          right side
+        </div>
+        
+      </div>
+    </div >
+  )
 }
 
-export default App;
+export default App
